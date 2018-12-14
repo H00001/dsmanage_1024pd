8a292d7bc148e00faabcc0434f3b34b5d3697282
H00001 <lnpj.office@icloud.com>
2018-12-14 11:50:57 +0800

    i love code

--- client/main.c
+++ client/main.c
@@ -4,11 +4,11 @@
 int port=6789;
 
 
-char ip[CLILEN_V4][16]={"127.0.0.1","",""};
-char id[CLILEN_V4] = {121,121,123};
+char ip[CLILEN_V4][16]={"127.0.0.1","192.168.1.103",""};
+char id[CLILEN_V4] = {121,120,123};
 int main(int argc, char** argv) 
 {
-        print_first();
+        //print_first();
 	int cpid = 0;
 	if((cpid=fork())==0)
 	{
@@ -21,6 +21,9 @@ int main(int argc, char** argv)
                 inint(&message);
                 message.code = 3;
                 while(1){
+                                //printf("sh #:");
+                                //fflush(stdout);
+
                                 fgets (message.message, MESSAGELEN-1, stdin);
                                 message.message[strlen(message.message)-1] = 0;
                                 if(strlen(message.message)==0||message.message[0]=='\n')
